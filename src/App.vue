594470480d2d9c61fc5aedc0e3f4788b904669f5
mikilaus <41994169+mikilaus@users.noreply.github.com>
2020-06-22 12:18:16 +0200

    e
    Signed-off-by: mikilaus <41994169+mikilaus@users.noreply.github.com>

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="mb-3">
+            <input type="text" v-model="theme">
+            <input type="number" v-model="offset">
+        </div>
+        <div>
+            <CounterButton :theme="theme" :offset="offset"></CounterButton>
+            <CounterButton :theme="theme" :offset="offset"></CounterButton>
+            <CounterButton :theme="theme" :offset="offset"></CounterButton>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import CounterButton from './CounterButton.vue';
+
+export default {
+    name: 'App',
+    components: { CounterButton },
+
+    data: function() {
+        return {
+            theme: 'success',
+            offset: 2
+        };
+    }
+};
+</script>
+
---
+++ src/CounterButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="Increase()"
+  :class="classes">{{ counterPlusOffset }}</button>
+</template>
+
+<script>
+export default {
+    props: ["theme", "offset"],
+    data:function(){
+        return {
+            counter:0
+        }
+    }, 
+    methods: {
+        Increase(){
+            this.counter++;
+        }
+    },
+    computed: {
+        classes(){
+           return ['btn', 'btn-' + this.theme];
+        },
+        counterPlusOffset(){
+            return this.counter + +this.offset;
+        }
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
